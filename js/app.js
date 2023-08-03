19401f3b06a4e6939c46ad44885b31278b67f0a6
Xanniy <danilbalobanov76@gmail.com>
2023-08-03 20:31:04 +0400

    color

--- js/app.js
+++ js/app.js
@@ -5,10 +5,10 @@ const beige = document.querySelector('#beige')
 const white = document.querySelector('#white')
 const blue = document.querySelector('#blue')
 const urls = {
-    brown: "url(../brown.jpg) 50%/ cover", 
-    beige: "url(../beige.jpg) 50%/ cover",
-    white: "url(../white.jpg) 50%/ cover",
-    blue: "url(../blue.jpg) 50%/ cover",
+    brown: "url(../img/brown.jpg) 50%/ cover", 
+    beige: "url(../img/beige.jpg) 50%/ cover",
+    white: "url(../img/white.jpg) 50%/ cover",
+    blue: "url(../img/blue.jpg) 50%/ cover",
 }
 const btnList = [brown, beige, white, blue]
 
